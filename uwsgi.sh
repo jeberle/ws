a114b66edfd3ebfd842ae80d9f7e7f57044e4e23
Jim Eberle <jim.eberle@fastnlight.com>
2014-07-12 10:40:04 -0700

    update uwsgi.sh

--- uwsgi.sh
+++ uwsgi.sh
@@ -2,7 +2,20 @@
 
 # brew install uwsgi --with-python
 
-[ $# -eq 2 ] || { echo "usage: $0 <host[:port]> <serve-dir>"; exit 1; }
+socket=${1:-127.0.0.1:9090}
+srvdir=${2:-.}
+shift 2
 
-uwsgi --http-socket "$1" --chdir2 "$2" --plugin python --wsgi-file wsgi.py
+uwsgi --http-socket "$socket" --chdir2 "$srvdir" --log-micros \
+  --master --processes 2 \
+  --plugin python --wsgi-file wsgi.py \
+  --plugin router_cache \
+  --cache2 'name=cache1,items=100' \
+  --route '\.css$ cache:key=${REQUEST_URI},name=cache1,content_type=text/css' \
+  --route '\.png$ cache:key=${REQUEST_URI},name=cache1,content_type=image/png' \
+  --route '.* cache:key=${REQUEST_URI},name=cache1' \
+  --route '.* cachestore:key=${REQUEST_URI},name=cache1,expires=60' \
+  "$@"
+
+# ./uwsgi.sh 127.0.0.1:9090 . --py-auto-reload 1
 

6157a4d0273e4cef0d9821c1cc9f5710a8766971
Jim Eberle <jim.eberle@fastnlight.com>
2014-07-12 10:57:36 -0700

    update uwsgi.sh

--- uwsgi.sh
+++ uwsgi.sh
@@ -13,8 +13,9 @@ uwsgi --http-socket "$socket" --chdir2 "$srvdir" --log-micros \
   --cache2 'name=cache1,items=100' \
   --route '\.css$ cache:key=${REQUEST_URI},name=cache1,content_type=text/css' \
   --route '\.png$ cache:key=${REQUEST_URI},name=cache1,content_type=image/png' \
+  --route '\.txt$ cache:key=${REQUEST_URI},name=cache1,content_type=text/plain; charset=utf-8' \
   --route '.* cache:key=${REQUEST_URI},name=cache1' \
-  --route '.* cachestore:key=${REQUEST_URI},name=cache1,expires=60' \
+  --route '.* cachestore:key=${REQUEST_URI},name=cache1,expires=15' \
   "$@"
 
 # ./uwsgi.sh 127.0.0.1:9090 . --py-auto-reload 1

1b81ecc89738bf5172b0d055aa579e093d7351db
Jim Eberle <jim.eberle@fastnlight.com>
2014-07-12 02:42:56 -0700

    update page.html, wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,3 +1,5 @@
+'''Provides Pygments and reStructuredText as a minimal WSGI application.'''
+
 import os.path
 import string
 
@@ -6,18 +8,24 @@ import pygments.lexers as lexers
 import pygments.formatters as formatters
 import docutils.core
 
+DIR = os.path.dirname(os.path.abspath(__file__))
+PAGE = string.Template(open(os.path.join(DIR, 'page.html')).read())
 LEX_MAP = {
     'sh': lexers.BashLexer(),
     'pl': lexers.PerlLexer(),
     'py': lexers.PythonLexer(),
-    'xml': lexers.XmlLexer(),
 }
-PAGE = string.Template(open('page.html').read())
 
 def application(env, start_response):
-    uri, content, resp, ctype = env['REQUEST_URI'], '', '200 OK', 'text/html'
+    resp, ctype, content = '200 OK', 'text/html; charset=utf-8', None
+    method, uri = env['REQUEST_METHOD'], env['REQUEST_URI']
     fname, title = uri[1:], uri[1:]
-    if not os.path.isfile(fname):
+    if fname.startswith('static/'):
+        fname = fname.replace('static', DIR)
+    if method != 'GET':
+        resp = '501 Not Implemented'
+        title, body = 'Not Implemented', '<h2>Not Implemented</h2>\n'
+    elif not os.path.isfile(fname):
         resp = '404 Not Found'
         title, body = 'File not found', '<h2>File not found</h2>\n'
     elif fname.endswith('.css'):
@@ -25,10 +33,10 @@ def application(env, start_response):
     elif fname.endswith('.txt'):
         ctype, content = 'text/plain', open(fname).read()
     elif fname.endswith('.rst') or fname.endswith('.rest'):
-        body = rst2html(fname)
+        body = rst2html(open(fname).read())
     else:
-        body = pygmentize(fname)
-    if not content:
+        body = pygmentize(open(fname).read())
+    if content is None:
         content = PAGE.substitute({'title':title, 'body':body})
     start_response(resp, [
         ('Content-Type', ctype),
@@ -36,11 +44,10 @@ def application(env, start_response):
     ])
     return [content]
 
-def rst2html(fname):
-    return docutils.core.publish_string(source=open(fname).read(), writer_name='html')
+def rst2html(buf):
+    return docutils.core.publish_string(source=buf, writer_name='html')
 
-def pygmentize(fname):
-    buf = open(fname).read()
+def pygmentize(buf):
     ext = fname.rsplit('.', -1)[1] if '.' in fname else ''
     lexer = LEX_MAP[ext] if ext in LEX_MAP else lexers.guess_lexer(buf)
     formatter = formatters.HtmlFormatter(linenos=False, style='vs')

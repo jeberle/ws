128066bac53543b5cdb1030c21c0b89eb4541ee5
Jim Eberle <jim.eberle@fastnlight.com>
2014-07-15 12:43:16 -0700

    update uwsgi.sh

--- uwsgi.sh
+++ uwsgi.sh
@@ -4,19 +4,20 @@
 # pip install -r requirements.txt
 # ./uwsgi.sh 127.0.0.1:9090 . 1 --py-auto-reload 1
 
-socket=${1:-127.0.0.1:9090}
-srvdir=${2:-.}
-expires=${3:-15}
+DIR=${0%/*}
+SOCKET=${1:-127.0.0.1:9090}
+SRVDIR=${2:-.}
+EXPIRES=${3:-15}
 shift 3
 
-uwsgi --http-socket "$socket" --chdir2 "$srvdir" --log-micros \
+uwsgi --http-socket "$SOCKET" --chdir2 "$SRVDIR" --log-micros \
   --master --processes 2 \
-  --plugin python --wsgi-file wsgi.py \
+  --plugin python --wsgi-file "$DIR/"wsgi.py \
   --plugin router_cache \
   --cache2 'name=cache1,items=100' \
   --route '\.css$ cache:key=${REQUEST_URI},name=cache1,content_type=text/css' \
   --route '\.png$ cache:key=${REQUEST_URI},name=cache1,content_type=image/png' \
   --route '.* cache:key=${REQUEST_URI},name=cache1,content_type=text/html;charset=utf-8' \
-  --route '.* cachestore:key=${REQUEST_URI},name=cache1,expires='"$expires" \
+  --route '.* cachestore:key=${REQUEST_URI},name=cache1,expires='"$EXPIRES" \
   "$@"
 

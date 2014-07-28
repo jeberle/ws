dc85f431a62054e6892ef2e1e33be18e5f66d582
Jim Eberle <jim.eberle@fastnlight.com>
2014-07-27 18:16:54 -0700

    update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -12,7 +12,7 @@ import pygments.lexers
 import pygments.formatters
 
 DIR = os.path.dirname(os.path.abspath(__file__))
-THEME = 'sphinx13'
+THEME = '.'
 
 def application(env, start_response):
     resp_code, doc = handle(env)
@@ -58,7 +58,7 @@ def showfile(root, fpath):
 
 def txt(root, fpath):
     buf = unicode(open(fpath).read(), encoding='utf-8')
-    return page(root, title(fpath), '<pre>%s</pre>\n' % cgi.escape(buf))
+    return page(root, title(fpath), pre(cgi.escape(buf)))
 
 def rst(root, fpath):
     parts = docutils.core.publish_parts(source=open(fpath).read(), writer_name='html',
@@ -82,19 +82,19 @@ def highlight(lexer):
 # --- directories ---
 
 def listdir(root, fpath):
-    m = h1(fpath)
-    m += '<table id="dirlist">\n'
+    body = [h1(fpath), table('dirlist', rows(root, fpath))]
+    return page(root, title(fpath), body)
+
+def rows(root, fpath):
     names = [ n for n in os.listdir(fpath) if not n.startswith('.') ]
     if fpath == '.':
         fpath = ''
     else:
         url = '%s/%s' % (root, os.path.dirname(fpath))
-        m += tr(td(a_href(url, '[..]')))
+        yield tr(td(a_href(url, '[..]')))
     for name in sorted(names, cmp_name):
         url = '%s/%s' % (root, os.path.join(fpath, name))
-        m += tr(td(a_href(url, name)))
-    m += '</table>\n'
-    return page(root, title(fpath), m)
+        yield tr(td(a_href(url, name)))
 
 def cmp_name(a, b):
     return cmp(os.path.isdir(b), os.path.isdir(a)) or cmp(a.lower(), b.lower())
@@ -126,27 +126,42 @@ def not_impl(root):
     return page(root, title('Not Implemented'), h2('Not Implemented'))
 
 def page(root, head, body):
-    head, body = head.encode('utf-8'), body.encode('utf-8')
+    head = ''.join(flatten(head)).encode('utf-8')
+    body = ''.join(flatten(body)).encode('utf-8')
     tmpl = string.Template(open(os.path.join(DIR, THEME, 'page.html')).read())
     return 'text/html', tmpl.substitute({'root':root, 'head':head, 'body':body})
 
 # --- tags ---
 
-def title(s):
-    return '<title>%s</title>\n' % s
+def title(a):
+    return ['<title>', a, '</title>\n']
 
-def h1(s):
-    return '<h1>%s</h1>\n' % s
+def h1(a):
+    return ['<h1>', a, '</h1>\n']
 
-def h2(s):
-    return '<h2>%s</h2>\n' % s
+def h2(a):
+    return ['<h2>', a, '</h2>\n']
 
-def tr(s):
-    return '<tr>%s</tr>\n' % s
+def table(tid, a):
+    return ['<table id="%s">\n' % tid, a, '</table>\n']
 
-def td(s):
-    return '<td>%s</td>' % s
+def tr(a):
+    return ['<tr>', a, '</tr>\n']
 
-def a_href(s, t):
-    return '<a href="%s">%s</a>' % (urllib.quote_plus(s, '/'), t)
+def td(a):
+    return ['<td>', a, '</td>']
+
+def a_href(url, a):
+    return ['<a href="%s">' % urllib.quote_plus(url, '/'), a, '</a>']
+
+def pre(a):
+    return ['<pre>', a, '</pre>\n']
+
+def flatten(a):
+    if type(a) in (type(''), type(u'')):
+        yield a
+    else:
+        for v in a:
+            for w in flatten(v):
+                yield w
 

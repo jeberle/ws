d353e501f55fd2e4c7dd4e0f83c473f8c73415e2
Jim Eberle <jim.eberle@fastnlight.com>
2014-08-24 18:31:02 -0700

    use jinja2 templates throughout

--- wsgi.py
+++ wsgi.py
@@ -1,11 +1,11 @@
 '''Render Pygments, reStructuredText, and Markdown via a minimal WSGI application.'''
 
 import os.path
-import string
+import re
 import cgi
 import urllib
 
-import docutils.core
+from docutils.core import publish_parts
 from markdown import markdown
 from textile import textile
 import pygments
@@ -17,7 +17,7 @@ import yaml
 DIR = os.path.dirname(os.path.abspath(__file__))
 THEME = '.'
 ENV = Environment(autoescape=True, trim_blocks=True, lstrip_blocks=True,
-    loader=FileSystemLoader('.'),
+    loader=FileSystemLoader(os.path.join(DIR, THEME)),
     extensions=['jinja2.ext.autoescape'])
 
 def application(env, start_response):
@@ -31,14 +31,14 @@ def application(env, start_response):
 
 def handle(env):
     if env['REQUEST_METHOD'] != 'GET':
-        return '501 Not Implemented', not_impl(root)
+        return '501 Not Implemented', page(root, 'Not Implemented', '')
     root, fpath = env['SCRIPT_NAME'], resolve(env['REQUEST_URI'])
     if os.path.isdir(fpath):
         return '200 OK', listdir(root, fpath)
     if os.path.isfile(fpath):
         return '200 OK', showfile(root, fpath)
     else:
-        return '404 Not Found', not_found(root)
+        return '404 Not Found', page(root, 'File not found', '')
 
 def resolve(uri):
     '''resolve URI -> fpath, w/ special case for "/static" stem'''
@@ -56,6 +56,8 @@ def resolve(uri):
 # --- files ---
 
 def showfile(root, fpath):
+    if os.path.basename(os.path.dirname(os.path.abspath(fpath))) == 'marks':
+        return marks(root, fpath)
     ext = '.' + fpath.rsplit('.', -1)[1] if '.' in fpath else ''
     if ext in EXT_MAP:
         return EXT_MAP[ext](root, fpath)
@@ -64,7 +66,7 @@ def showfile(root, fpath):
 
 def txt(root, fpath):
     buf = unicode(open(fpath).read(), encoding='utf-8')
-    return page(root, title(fpath), pre(cgi.escape(buf)))
+    return page(root, fpath, '<pre>%s</pre>' % cgi.escape(buf))
 
 def yml(root, fpath):
     buf = unicode(open(fpath).read(), encoding='utf-8')
@@ -73,16 +75,16 @@ def yml(root, fpath):
     return 'text/html', tmpl.render(d).encode('utf-8')
 
 def rst(root, fpath):
-    parts = docutils.core.publish_parts(source=open(fpath).read(), writer_name='html',
+    parts = publish_parts(source=open(fpath).read(), writer_name='html',
         settings_overrides={'syntax_highlight': 'short', 'math_output': 'MathML'})
     return page(root, parts['head'], parts['html_body'])
 
 def md(root, fpath):
-    return page(root, title(fpath), markdown(open(fpath).read()))
+    return page(root, fpath, markdown(open(fpath).read()))
 
 def txtl(root, fpath):
     buf = textile(open(fpath).read(), html_type='html', auto_link=True, encoding='utf-8')
-    return page(root, title(fpath), buf)
+    return page(root, fpath, buf)
 
 def cat(ctype):
     def f(root, fpath):
@@ -92,14 +94,13 @@ def cat(ctype):
 def highlight(lexer):
     def f(root, fpath):
         body = pygments.highlight(open(fpath).read(), lexer, FORMATTER)
-        return page(root, title(fpath), body)
+        return page(root, fpath, body)
     return f
 
 # --- directories ---
 
 def listdir(root, fpath):
-    body = [h1(fpath), table('dirlist', rows(root, fpath))]
-    return page(root, title(fpath), body)
+    return render(root, fpath, {'rows': rows(root, fpath)}, 'dirlist.html')
 
 def rows(root, fpath):
     names = [ n for n in os.listdir(fpath) if not n.startswith('.') ]
@@ -107,13 +108,41 @@ def rows(root, fpath):
         fpath = ''
     else:
         url = '%s/%s' % (root, os.path.dirname(fpath))
-        yield tr(td(a_href(url, '[..]')))
+        yield {'url': urllib.quote_plus(url, '/'), 'name': '[..]'}
     for name in sorted(names, cmp_name):
         url = '%s/%s' % (root, os.path.join(fpath, name))
-        yield tr(td(a_href(url, name)))
+        yield {'url': urllib.quote_plus(url, '/'), 'name': name}
 
 def cmp_name(a, b):
     return cmp(os.path.isdir(b), os.path.isdir(a)) or cmp(a.lower(), b.lower())
+
+# --- bookmarks ---
+
+def marks(root, fpath):
+    title, sections = load_marks(fpath)
+    return render(root, title, {'sections': sections}, 'marks.html')
+
+def load_marks(fpath):
+    f = open(fpath)
+    title = unicode(f.readline().rstrip(), encoding='utf-8')
+    sections = []
+    sec = {'hdr': '', 'links': []}
+    for line in f:
+        line = unicode(line.rstrip(), encoding='utf-8')
+        if re.search(r'(^-)|(^[#])', line):  # comment line
+            continue
+        elif line == '':  # empty line
+            if (sec['hdr'] != ''):
+                sections.append(sec)
+            sec = {'hdr': '', 'links': []}
+        elif (sec['hdr'] == ''):  # group line
+            sec['hdr'] = line
+        else:  # link line
+          m = re.match(r'^(.*)\t(.*)$', line)
+          name = m.group(1)
+          url  = m.group(2)
+          sec['links'].append({'name': name, 'url': url})
+    return title, sections
 
 # --- etc ---
 
@@ -138,49 +167,12 @@ EXT_MAP = {
 }
 FORMATTER = pygments.formatters.HtmlFormatter(linenos=False, style='vs')
 
-def not_found(root):
-    return page(root, title('File not found'), h2('File not found'))
+def page(root, title, body):
+    return render(root, title, {'body': body.encode('utf-8')}, 'page.html')
 
-def not_impl(root):
-    return page(root, title('Not Implemented'), h2('Not Implemented'))
-
-def page(root, head, body):
-    head = ''.join(flatten(head)).encode('utf-8')
-    body = ''.join(flatten(body)).encode('utf-8')
-    tmpl = string.Template(open(os.path.join(DIR, THEME, 'page.html')).read())
-    return 'text/html', tmpl.substitute({'root':root, 'head':head, 'body':body})
-
-# --- tags ---
-
-def title(a):
-    return ['<title>', a, '</title>\n']
-
-def h1(a):
-    return ['<h1>', a, '</h1>\n']
-
-def h2(a):
-    return ['<h2>', a, '</h2>\n']
-
-def table(tid, a):
-    return ['<table id="%s">\n' % tid, a, '</table>\n']
-
-def tr(a):
-    return ['<tr>', a, '</tr>\n']
-
-def td(a):
-    return ['<td>', a, '</td>']
-
-def a_href(url, a):
-    return ['<a href="%s">' % urllib.quote_plus(url, '/'), a, '</a>']
-
-def pre(a):
-    return ['<pre>', a, '</pre>\n']
-
-def flatten(a):
-    if type(a) in (type(''), type(u'')):
-        yield a
-    else:
-        for v in a:
-            for w in flatten(v):
-                yield w
+def render(root, title, ctx, tmplname):
+    ctx['root'] = root
+    ctx['title'] = title
+    tmpl = ENV.get_template(tmplname)
+    return 'text/html', tmpl.render(ctx).encode('utf-8')
 

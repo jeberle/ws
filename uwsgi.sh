8feffa3fa522e9b8df620301ba20392a406754e5
Jim Eberle <jim.eberle@fastnlight.com>
2014-09-06 01:14:40 -0700

    update uwsgi.sh

--- uwsgi.sh
+++ uwsgi.sh
@@ -16,7 +16,12 @@ uwsgi --http-socket "$SOCKET" --chdir2 "$SRVDIR" \
   --plugin router_cache \
   --cache2 'name=cache1,items=100' \
   --route '\.css$ cache:key=${REQUEST_URI},name=cache1,content_type=text/css' \
+  --route '\.xml$ cache:key=${REQUEST_URI},name=cache1,content_type=text/xml' \
+  --route '\.gif$ cache:key=${REQUEST_URI},name=cache1,content_type=image/gif' \
   --route '\.png$ cache:key=${REQUEST_URI},name=cache1,content_type=image/png' \
+  --route '\.jpg$ cache:key=${REQUEST_URI},name=cache1,content_type=image/jpeg' \
+  --route '\.svg$ cache:key=${REQUEST_URI},name=cache1,content_type=image/svg+xml' \
+  --route '\.js$ cache:key=${REQUEST_URI},name=cache1,content_type=application/x-javascript' \
   --route '.* cache:key=${REQUEST_URI},name=cache1,content_type=text/html' \
   --route '.* cachestore:key=${REQUEST_URI},name=cache1,expires='"$EXPIRES" \
   --log-format '%(status) %(msecs) %(method) %(uri)' \

c7535cfb62183be26a0234cfb8a4d5d4d9e43ee7
Jim Eberle <jim.eberle@fastnlight.com>
2014-09-06 18:36:14 -0700

    update txtl.py

--- txtl.py
+++ txtl.py
@@ -7,7 +7,7 @@ def txtl(fpath):
     return '', textile(open(fpath).read(), html_type='html', auto_link=True, encoding='utf-8')
 
 class Textile(Extension):
-    '''add {% txtl %}...{% endtxtl %} custom tag'''
+    '''add {% txtl 'nl2sp' %}...{% endtxtl %} custom tag'''
     tags = set(['txtl'])
 
     def __init__(self, environment):
@@ -15,9 +15,20 @@ class Textile(Extension):
 
     def parse(self, parser):
         lineno = parser.stream.next().lineno
+        if parser.stream.current.test('string'):
+            nl2sp = parser.parse_expression()
+        else:
+            nl2sp = nodes.Const(None)
         body = parser.parse_statements(['name:endtxtl'], drop_needle=True)
-        return nodes.CallBlock(self.call_method('_render'), [], [], body).set_lineno(lineno)
+        return nodes.CallBlock(self.call_method('_render', [nl2sp]), [], [], body).set_lineno(lineno)
 
-    def _render(self, caller):
-        return textile(caller().encode('utf-8'), html_type='html', auto_link=True, encoding='utf-8')
+    def _render(self, nl2sp, caller):
+        txt = _nl2sp(caller()) if nl2sp == 'nl2sp' else caller()
+        return textile(txt.encode('utf-8'), html_type='html', auto_link=True, encoding='utf-8')
+
+def _nl2sp(txt):
+    txt = txt.replace('\n\n', '\0')
+    txt = txt.replace('\n', ' ')
+    txt = txt.replace('\0', '\n')
+    return txt
 

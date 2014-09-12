69cd4276b55482f5cf49eff76dc3d79932dffa46
Jim Eberle <jim.eberle@fastnlight.com>
2014-09-12 02:07:51 -0400

    issue 301 for dir refs w/o trailing /

--- wsgi.py
+++ wsgi.py
@@ -25,42 +25,54 @@ EXT_MAP = {
     '.js': 'application/x-javascript; charset=utf-8',
 }
 
+HTML = 'text/html; charset=utf-8'
+XML = 'text/xml; charset=utf-8'
+
 def application(env, start_response):
-    resp_code, ctype, content = handle(env)
-    start_response(resp_code, [
-        ('Content-Type', ctype),
-        ('Content-Length', str(len(content)))
-    ])
+    resp_code, ctype, loc, content = handle(env)
+    if loc:
+        start_response(resp_code, [
+            ('Content-Type', ctype),
+            ('Content-Length', str(len(content))),
+            ('Location', loc)
+        ])
+    else:
+        start_response(resp_code, [
+            ('Content-Type', ctype),
+            ('Content-Length', str(len(content)))
+        ])
     return [content]
 
 def handle(env):
     try:
         root, method, uri = env['SCRIPT_NAME'], env['REQUEST_METHOD'], env['REQUEST_URI']
         if method != 'GET':
-            return '501 Not Implemented', 'text/html; charset=utf-8', error(root, 'Not Implemented')
+            return '501 Not Implemented', HTML, None, error(root, 'Not Implemented')
         fpath = resolve(root, uri)
         ext = '.' + fpath.rsplit('.', 1)[1] if '.' in fpath else ''
         if ext in EXT_MAP and os.path.isfile(fpath):
-            return '200 OK', EXT_MAP[ext], open(fpath).read()
+            return '200 OK', EXT_MAP[ext], None, open(fpath).read()
         if os.path.isdir(fpath):
+            if not uri.endswith('/'):
+                return '301 Moved Permanently', HTML, uri+'/', error(root, 'Moved Permanently')
             # check for index.* w/in dir
             ls = sorted(glob(os.path.join(fpath, 'index.*')))
             if ls:
                 fpath = ls[0]
             else:
-                return '200 OK', 'text/html; charset=utf-8', dirlist(root, fpath)
+                return '200 OK', HTML, None, dirlist(root, fpath)
         elif not os.path.exists(fpath):
             # try to resolve file like Apache MultiViews: /foo -> /foo.html
             ls = sorted(glob(fpath + '.*'))
             if ls:
                 fpath = ls[0]
         if os.path.isfile(fpath):
-            ctype = 'text/%s; charset=utf-8' % ('xml' if fpath.endswith('.xml') else 'html')
-            return '200 OK', ctype, showfile(root, fpath)
+            ctype = XML if fpath.endswith('.xml') else HTML
+            return '200 OK', ctype, None, showfile(root, fpath)
         else:
-            return '404 Not Found', 'text/html; charset=utf-8', error(root, 'File not found')
+            return '404 Not Found', HTML, None, error(root, 'File not found')
     except Exception as ex:
-        return '500 Error', 'text/html; charset=utf-8', error(root, traceback.format_exc())
+        return '500 Error', HTML, None, error(root, traceback.format_exc())
 
 def resolve(root, uri):
     '''resolve URI -> fpath, w/ special case for "<root>/ws" stem'''

5fffbcdadf533a2d860cd73ec04188e1d8ae8e55
Jim Eberle <jim.eberle@fastnlight.com>
2014-09-12 01:27:37 -0400

    rm mirror.sh

--- mirror.sh
+++
@@ -1,7 +0,0 @@
-#! /usr/bin/env bash
-
-set -e
-rm -r _site
-mkdir _site
-wget -r --no-host-directories -P _site http://localhost:9090
-find _site -type f ! -name '*.*' -print0 | xargs -0 -n 1 bash -c 'mv $0 $0.html'
